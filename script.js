da7a5dcc630770dd71c1f70ea299624728d3dc25
Bruno Fabrício Eufrásio <brunofabricioeufrasio@gmail.com>
2023-05-03 20:37:13 -0300

    agora com input ranger o usuario pode  definir o percentual que quer investir

--- script.js
+++ script.js
@@ -9,47 +9,57 @@ document.getElementById("mercado").addEventListener("input", atualizarSaldo);
 document.getElementById("cartaoCredito").addEventListener("input", atualizarSaldo);
 document.getElementById("outrasDespesas").addEventListener("input", atualizarSaldo);
 
+const pinvestInput = document.getElementById("pinvest");
+const pinvestOutput = document.getElementById("pinvestOutput");
 
+pinvestInput.addEventListener("change", () => {
+  pinvestOutput.innerText = pinvestInput.value;
+  atualizarSaldo();
+});
 
 function atualizarSaldo() {
-// Recupera os valores dos campos de entrada
-var salario = parseFloat(document.getElementById("salario").value) || 0;
-var outrasReceitas = parseFloat(document.getElementById("outrasReceitas").value) || 0;
-var agua = parseFloat(document.getElementById("agua").value) || 0;
-var aluguel = parseFloat(document.getElementById("aluguel").value) || 0;
-var energia = parseFloat(document.getElementById("energia").value) || 0;
-var internet = parseFloat(document.getElementById("internet").value) || 0;
-var mercado = parseFloat(document.getElementById("mercado").value) || 0;
-var cartaoCredito = parseFloat(document.getElementById("cartaoCredito").value) || 0;
-var outrasDespesas = parseFloat(document.getElementById("outrasDespesas").value) || 0;
+  // Recupera os valores dos campos de entrada
+  var salario = parseFloat(document.getElementById("salario").value) || 0;
+  var outrasReceitas = parseFloat(document.getElementById("outrasReceitas").value) || 0;
+  var agua = parseFloat(document.getElementById("agua").value) || 0;
+  var aluguel = parseFloat(document.getElementById("aluguel").value) || 0;
+  var energia = parseFloat(document.getElementById("energia").value) || 0;
+  var internet = parseFloat(document.getElementById("internet").value) || 0;
+  var mercado = parseFloat(document.getElementById("mercado").value) || 0;
+  var cartaoCredito = parseFloat(document.getElementById("cartaoCredito").value) || 0;
+  var outrasDespesas = parseFloat(document.getElementById("outrasDespesas").value) || 0;
 
-// Realiza as operações de adição e subtração
-var totalReceitas = salario + outrasReceitas;
-var totalDespesas = agua + aluguel + energia + internet + mercado + cartaoCredito + outrasDespesas;
-var saldo = totalReceitas - totalDespesas;
-var saldoInvestimento = saldo > 0 ? saldo : 0;
+  // Recupera o percentual de investimento
+  var percentualInvestimento = parseFloat(document.getElementById("pinvest").value) || 0;
 
-// Subtrai 30% do saldo e adiciona ao saldo de investimento
-var saldoInvestimento = saldo * 0.3;
-saldo -= saldoInvestimento;
+  // Realiza as operações de adição e subtração
+  var totalReceitas = salario + outrasReceitas;
+  var totalDespesas = agua + aluguel + energia + internet + mercado + cartaoCredito + outrasDespesas;
+  var saldo = totalReceitas - totalDespesas;
 
-// Exibe os resultados nos campos de saída correspondentes
-document.getElementById("saldo").value = saldo.toFixed(2);
-document.getElementById("saldoInvestimento").value = saldoInvestimento.toFixed(2);
+  // Calcula o saldo de investimento com base no percentual definido pelo usuário
+  var saldoInvestimento = saldo * (percentualInvestimento / 100);
+
+  // Subtrai o saldo de investimento do saldo total
+  saldo -= saldoInvestimento;
+
+  // Exibe os resultados nos campos de saída correspondentes
+  document.getElementById("saldo").value = saldo.toFixed(2);
+  document.getElementById("saldoInvestimento").value = saldoInvestimento.toFixed(2);
 }
 
 function limparCampos() {
-// redefine os valores dos campos de entrada e saída
-document.getElementById("salario").value = "placeholder";
-document.getElementById("outrasReceitas").value = "placeholder";
-document.getElementById("agua").value = "placeholder";
-document.getElementById("aluguel").value = "placeholder";
-document.getElementById("energia").value = "placeholder";
-document.getElementById("internet").value = "placeholder";
-document.getElementById("mercado").value = "placeholder";
-document.getElementById("cartaoCredito").value = "placeholder";
-document.getElementById("outrasDespesas").value = "placeholder";
-document.getElementById("saldo").value = "placeholder";
+  // redefine os valores dos campos de entrada e saída
+  document.getElementById("salario").value = "";
+  document.getElementById("outrasReceitas").value = "";
+  document.getElementById("agua").value = "";
+  document.getElementById("aluguel").value = "";
+  document.getElementById("energia").value = "";
+  document.getElementById("internet").value = "";
+  document.getElementById("mercado").value = "";
+  document.getElementById("cartaoCredito").value = "";
+  document.getElementById("outrasDespesas").value = "";
+  document.getElementById("saldo").value = "";
 document.getElementById("saldoInvestimento").value = "placeholder";
 } 
 // Chama a função atualizarSaldo ao carregar a página
